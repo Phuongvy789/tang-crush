620a71052709a47a0e6dc58adf1507fc882f5643
Phuongvy789 <93928799+Phuongvy789@users.noreply.github.com>
2021-11-08 22:19:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim chị lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, Em Gái à :3',
+    btnYes: 'Vẫn cứ là thích chị <33',
+    btnNo: 'Không, Chị Gái à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối mai 7h chị qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/100072630161416'
 }
